c5398c0d441d64e696ebc3003c9917aad5988698
adityadixit-dev <adityadixit@gmail.com>
2025-06-04 18:33:16 +0700

    Ch4 L4 - Happy about how clean it is. Maybe i am wrong

--- src/api/validate_chirp.ts
+++ src/api/validate_chirp.ts
@@ -1,20 +1,55 @@
 import { Request, Response } from "express";
 import { respondWithError, respondWithJSON } from "../utils/json_resp.js";
 
+type Parameters = { body: string };
+
 export const handlerValidateChirp = async (req: Request, res: Response) => {
-  type parameters = { body: string };
+  const params: Parameters = req.body;
 
-  try {
-    const params: parameters = req.body;
-    const maxChirpLength = 140;
-    if (params.body.length > maxChirpLength) {
-      respondWithError(res, 400, "Chirp is too long");
-      return;
-    }
-
-    respondWithJSON(res, 200, { valid: true });
-  } catch (e) {
-    respondWithError(res, 400, "Invalid JSON");
+  if (isBodyUndefined(params, res)) {
     return;
   }
+
+  if (isChirpTooLong(params, res)) {
+    return;
+  }
+
+  const cleanedBody: string = cleanInputBody(params.body);
+
+  respondWithJSON(res, 200, { cleanedBody: cleanedBody });
 };
+
+function cleanInputBody(bodyStr: string) {
+  //.
+  const badWords = ["kerfuffle", "sharbert", "fornax"];
+  const bleeperWord = "****";
+  const wordsInBody = bodyStr.split(" ");
+  const cleanedMessage: string[] = [];
+
+  for (const word of wordsInBody) {
+    if (badWords.includes(word.toLowerCase())) {
+      cleanedMessage.push(bleeperWord);
+    } else {
+      cleanedMessage.push(word);
+    }
+  }
+
+  return cleanedMessage.join(" ");
+}
+
+function isChirpTooLong(params: Parameters, res: Response) {
+  const maxChirpLength = 140;
+  if (params.body.length > maxChirpLength) {
+    respondWithError(res, 400, "Chirp is too long");
+    return true;
+  }
+  return false;
+}
+
+function isBodyUndefined(params: Parameters, res: Response) {
+  if (!params.body || typeof params.body !== "string") {
+    respondWithError(res, 400, "Invalid Request body missing or body type is wrong");
+    return true;
+  }
+  return false;
+}
